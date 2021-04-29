4d0fdf0bbdb2832c5dd1db88aa3ca8ca012339e8
Allan <jdll.allan@gmail.com>
2021-04-29 13:30:41 +0200

    add required for name and weight

--- models/chicken.js
+++ models/chicken.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const db = require("../services/mongoose")
 
 const chickenSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     birthday: Date,
-    weight: Number,
+    weight: { type: Number, required: true },
     steps: { type: Number, default: 0 },
     isRunning: { type: Boolean, default: false }
 });
